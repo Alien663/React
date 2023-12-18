74c1dde1365348b12f195ad4c4aa801975c5b314
Alien663 <rite2844@gmail.com>
2023-12-18 08:04:44 +0800

    style:Add Account's Icon

--- src/Component/Bar/AppBar.jsx
+++ src/Component/Bar/AppBar.jsx
@@ -6,18 +6,18 @@ import MyIcon from '../MyIcon';
 import '../../Style/Bar.css'
 
 function AppBar(props) {
-  const {handleShowList} = props
+  const { handleShowList } = props
   const AccontName = "Alien663"
   return (
     <>
       <Navbar className='App-Bar' bg="primary" data-bs-theme="dark" fixed='top' expand="lg" >
         <div id="app-bar-background" className='app-bar-background'></div>
         <Container as="div" className='app-bar-container' fluid={true}>
-          <Row style={{width: "100%"}}>
+          <Row style={{ width: "100%" }}>
             <Col md={4}>
               <Nav>
                 <Nav.Link onClick={handleShowList}>
-                  <MyIcon iconName="List" color= "white" size={30}/>
+                  <MyIcon iconName="List" color="white" size={30} />
                 </Nav.Link>
 
                 <Navbar.Brand as={Link} to="/" >
@@ -27,10 +27,16 @@ function AppBar(props) {
             </Col>
             <Col md={4}></Col>
             <Col md={4}>
-              <div style={{justifyContent: "flex-end", textAlign: 'right'}}>
-                <DropdownButton id="dropdown-menu" align="end" title={AccontName}>
+              <div style={{ justifyContent: "flex-end", textAlign: 'right' }}>
+
+                <DropdownButton id="dropdown-menu" align="end" title={
+                  <span>
+                    <MyIcon iconName="PersonCircle" size={23} style={{ marginRight: "10px" }} />
+                    {AccontName}
+                  </span>
+                }>
                   <Dropdown.Item eventKey={1} as={Link} to="#hello">Profile</Dropdown.Item>
-                  <Dropdown.Divider/>
+                  <Dropdown.Divider />
                   <Dropdown.Item eventKey={2} as={Link} to="#hello">Logout</Dropdown.Item>
                 </DropdownButton>
               </div>

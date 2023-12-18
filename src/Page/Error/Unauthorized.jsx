60d9b74d69e4ce6d53751748dcd37b1bf95a8a52
Alien663 <rite2844@gmail.com>
2023-12-18 11:55:46 +0800

    feat:Add Http 401 Error

---
+++ src/Page/Error/Unauthorized.jsx
@@ -0,0 +1,11 @@
+const Unauthorized = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h2>Sorry, you are unauthorized to this page</h2>
+      <img src='https://http.cat/401' alt="User is Unauthorized"></img>
+      <h3>FamilyMart : There's no cat's door</h3>
+    </div>
+  )
+}
+
+export default Unauthorized
